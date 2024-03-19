17466f0b999914097c2b5648d9445fbf8726cee7
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2024-03-19 10:18:42 +0100

    PrimaryButton

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import './styles/App.scss'
 
 export default function App() {
   return (
+    <>
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    </>
   )
 }
---
+++ src/components/Buttons/PrimaryButton/index.tsx
@@ -0,0 +1,5 @@
+export function PrimaryButton() {
+  return (
+    <button className="btn py-2 px-10 rounded-lg bg-color-primary text-white border-2 border-color-primary hover:bg-white hover:text-color-primary hover:border-color-primary hover:border-2">En savoir plus</button>
+  )
+}
