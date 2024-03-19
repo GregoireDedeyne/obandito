50ba5267d7c1f4e4e4c1937fc098edcf05608ae1
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2024-03-19 10:30:36 +0100

    SecondaryButton

--- src/components/Buttons/PrimaryButton/index.tsx
+++ src/components/Buttons/PrimaryButton/index.tsx
@@ -1,5 +1,5 @@
 export function PrimaryButton() {
   return (
-    <button className="btn py-2 px-10 rounded-lg bg-color-primary text-white border-2 border-color-primary hover:bg-white hover:text-color-primary hover:border-color-primary hover:border-2">En savoir plus</button>
+    <button className="btn py-2 px-5 rounded-lg bg-color-primary text-white border-2 border-color-primary transition duration-150 hover:bg-white hover:text-color-primary hover:border-color-primary hover:border-2">En savoir plus</button>
   )
 }
---
+++ src/components/Buttons/SecondaryButton/index.tsx
@@ -0,0 +1,5 @@
+export function SecondaryButton() {
+  return (
+    <button className="btn py-2 px-5 rounded-lg bg-color-gray_light text-black border-2 border-color-gray_light transition duration-150 hover:bg-color-secondary hover:text-black hover:border-color-secondary hover:border-2">connexion</button>
+  )
+}
