5b959b4299c823e4700d6905c8fa09be7eae522e
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2024-03-22 13:20:33 +0100

    Fix responsive of login page

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -3,30 +3,26 @@ import { InputField } from '../InputField';
 import { PrimaryButton } from '../Buttons/PrimaryButton';
 import Image from '../../assets/images/bandPict3.jpg';
 import { NavLink } from 'react-router-dom';
-export function LoginForm({ onSubmit }) {
-  // const [formData, setFormData] = useState({});
 
-  // const handleChange = (e, fieldName) => {
-  //   setFormData({ ...formData, [fieldName]: e.target.value });
-  // };
+export function LoginForm({ onSubmit }) {
+  const [formData, setFormData] = useState({});
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
     onSubmit(formData);
   };
 
   return (
-    <div className="flex justify-between">
+    <div className="flex flex-col justify-center items-center sm:flex-row container mx-auto">
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col mr-48 ml-48 mt-64  items-center"
+        className="flex flex-col items-center sm:items-start m-4 w-[300px] lg:w-[500px]"
       >
         <InputField inputName="email" />
         <InputField inputName="password" />
 
-        <div className="my-2 text-center">
-          <PrimaryButton href="/home:id" text="Se connecter" />
+        <div className="my-2 text-center sm:text-left">
+          <PrimaryButton text="Se connecter" />
           <nav className="text-gray-500">
             {`Pas encore inscrit ? par `}
             <NavLink to="/subscribe" className="link">
@@ -35,8 +31,12 @@ export function LoginForm({ onSubmit }) {
           </nav>
         </div>
       </form>
-      <div className="mt-32 ml-48 mr-48 mb-32 ">
-        <img className="rounded-2xl w-screen" src={Image} />
+      <div className="m-4">
+        <img
+          className="rounded-2xl w-[300px] lg:w-[500px]"
+          src={Image}
+          alt="band"
+        />
       </div>
     </div>
   );
