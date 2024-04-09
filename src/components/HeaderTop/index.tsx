09a5be8cad72d08441b4e036f5cd83049a417f12
packonn <julien.bouloux@gmail.com>
2024-04-09 18:55:07 +0200

    new header

--- src/components/HeaderTop/index.tsx
+++ src/components/HeaderTop/index.tsx
@@ -1,11 +1,12 @@
+import { Fragment } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../store/redux-hook';
 import { logout } from '../../store/actions';
-
-interface GenreItemProps {
-  genre: string;
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
 }
-
 const GenreItem: React.FC<GenreItemProps> = ({ genre }) => {
   return (
     <NavLink to={`/genre/${genre}`}>
@@ -18,23 +19,19 @@ const GenreItem: React.FC<GenreItemProps> = ({ genre }) => {
           }
         }}
       >
-        {' '}
-        {genre}{' '}
-      </div>{' '}
+        {genre}
+      </div>
     </NavLink>
   );
 };
-
 export function Header({ genres }) {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
   const islogged = useAppSelector((state) => state.decodedToken.islogged);
-  const img_irl = useAppSelector(
-    (state) => state.decodedToken.decodedData.image_url
+  const { id, image_url, name, role, mail } = useAppSelector(
+    (state) => state.decodedToken.decodedData
   );
-  const id = useAppSelector((state) => state.decodedToken.decodedData.id);
-  const role = useAppSelector((state) => state.decodedToken.decodedData.role);
 
   const handleLogout = () => {
     dispatch(logout());
@@ -42,151 +39,247 @@ export function Header({ genres }) {
 
     navigate('/', { replace: true });
   };
-
   return (
-    <div className="flex flex-col justify-center bg-white ">
-      <header className="flex flex-col md:flex-row gap-5 justify-between self-center pt-3.5 w-full container ">
-        <div className="flex gap-5  justify-between self-start px-5 flex-grow max-w-[100%] xl:max-w-[50%]  md:w-auto w-screen ">
-          <NavLink to={'/'}>
-            <div className="text-2xl font-semibold leading-10 text-purple-800 pt-1 ">
-              <span className="text-rose-500">O'</span>Bandito
-            </div>
-          </NavLink>
-          <div className="dropdown md:hidden flex self-end		">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h7"
-                />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <NavLink to={'/subscribe'}>S'inscrire</NavLink>
-              </li>
-              <li>
-                <NavLink to={'/login'}>Se connecter</NavLink>
-              </li>
-            </ul>
-          </div>
-          {islogged === false ? (
-            <nav className="hidden md:flex gap-4 pr-20 my-auto text-sm font-medium leading-5 text-slate-900 ">
-              <ul className="menu menu-horizontal px-1 ">
-                <li
-                  className="hover:bg-rose-500 hover:text-white rounded-lg "
-                  onClick={() => {
-                    const events = document.getElementById('events');
-                    if (events) {
-                      events.scrollIntoView({ behavior: 'smooth' });
-                    }
-                  }}
-                >
-                  {' '}
-                  <a className="text-[10px] lg:text-sm">Evènements</a>
-                </li>
-                <li
-                  className="hover:bg-rose-500 hover:text-white rounded-lg"
-                  onClick={() => {
-                    const bands = document.getElementById('bands');
-                    if (bands) {
-                      bands.scrollIntoView({ behavior: 'smooth' });
-                    }
-                  }}
-                >
-                  {' '}
-                  <a className="text-[10px] lg:text-sm">Groupes</a>{' '}
-                </li>
-                <li
-                  className="hover:bg-rose-500 hover:text-white hover:rounded-lg"
-                  onClick={() => {
-                    const propos = document.getElementById('propos');
-                    if (propos) {
-                      propos.scrollIntoView({ behavior: 'smooth' });
-                    }
-                  }}
-                >
-                  {' '}
-                  <a className="text-[10px] lg:text-sm">à propos</a>{' '}
-                </li>
-              </ul>
-            </nav>
-          ) : null}
-        </div>
-        {islogged === false ? (
-          <div className="hidden md:flex flex-col md:flex-row justify-center items-center w-full max-w-[50%] md:w-auto">
-            <NavLink
-              to={'/subscribe'}
-              className={({ isActive }) =>
-                isActive
-                  ? 'hidden'
-                  : 'justify-center px-6 py-2.5 text-center text-white whitespace-nowrap bg-rose-500 rounded-[36px] md:px-5 hover:bg-purple-800'
-              }
-            >
-              M'inscrire
-            </NavLink>
-            <NavLink
-              to={'/login'}
-              className={({ isActive }) =>
-                isActive
-                  ? 'hidden'
-                  : 'justify-center self-center px-5 py-2 hover:border-b hover:border-rose-500 hover:border-solid text-slate-900'
-              }
-            >
-              Me connecter
-            </NavLink>
-          </div>
-        ) : (
-          <div className="flex justify-center">
-            <NavLink
-              to={
-                role === 'Artiste'
-                  ? `/profile/band/${id}`
-                  : `/profile/organizer/${id}`
-              }
-              className={({ isActive }) =>
-                isActive
-                  ? 'hidden'
-                  : 'justify-center px-6 py-2.5 self-center text-white whitespace-nowrap bg-rose-500 rounded-[36px] md:px-5 hover:bg-purple-800'
-              }
-            >
-              Mon profil
-            </NavLink>
-            <button
-              onClick={handleLogout}
-              className="justify-center self-center px-5 py-2 hover:border-b hover:border-rose-500 hover:border-solid text-slate-900"
-            >
-              {' '}
-              Se déconnecter{' '}
-            </button>
-            <div className="avatar ml-2">
-              <div className="w-14 rounded-full">
-                <img src={`${import.meta.env.VITE_BACK_URL}${img_irl}`} />
+    <div>
+      <Disclosure as="nav" className="bg-white shadow">
+        {({ open }) => (
+          <>
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <div className="flex  items-center h-16 justify-between">
+                <div className="flex items-baseline">
+                  <div className="flex flex-shrink-0 items-center">
+                    <NavLink
+                      to={'/'}
+                      className="flex flex-shrink-0 items-center text-2xl font-bold leading-10 text-purple-800"
+                    >
+                      <span className="  text-rose-500">O'</span>
+                      Bandito
+                    </NavLink>
+                  </div>
+                  <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+                    {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+                    <nav
+                      onClick={() => {
+                        const events = document.getElementById('events');
+                        if (events) {
+                          events.scrollIntoView({ behavior: 'smooth' });
+                        }
+                      }}
+                      className="inline-flex items-center h-fit hover:bg-rose-500 rounded-md p-3 py-2 text-sm font-medium cursor-pointer hover:text-white text-gray-900"
+                    >
+                      Évenements
+                    </nav>
+                    <a
+                      onClick={() => {
+                        const bands = document.getElementById('bands');
+                        if (bands) {
+                          bands.scrollIntoView({ behavior: 'smooth' });
+                        }
+                      }}
+                      className="inline-flex items-center h-fit hover:bg-rose-500 rounded-md px-3 py-2 text-sm font-medium cursor-pointer hover:text-white text-gray-900"
+                    >
+                      Groupes
+                    </a>
+                    <a
+                      onClick={() => {
+                        const propos = document.getElementById('propos');
+                        if (propos) {
+                          propos.scrollIntoView({ behavior: 'smooth' });
+                        }
+                      }}
+                      className="inline-flex items-center h-fit hover:bg-rose-500 rounded-md p-3 py-2 text-sm font-medium cursor-pointer hover:text-white text-gray-900"
+                    >
+                      À propos
+                    </a>
+                  </div>
+                </div>
+                <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                  {/* <button
+                    type="button"
+                    className="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    <span className="absolute -inset-1.5" />
+                    <span className="sr-only">View notifications</span>
+                    <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  </button> */}
+                  {/* Profile dropdown */}
+                  {islogged === true ? (
+                    <Menu as="div" className="relative ml-3">
+                      <div>
+                        <Menu.Button className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                          <span className="absolute -inset-1.5" />
+                          <span className="sr-only">Open user menu</span>
+                          <img
+                            className="h-10 w-10 rounded-full"
+                            src={`${import.meta.env.VITE_BACK_URL}${image_url}`}
+                            alt="image profil"
+                          />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-200"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <NavLink
+                                to={
+                                  role === 'Artiste'
+                                    ? `/profile/band/${id}`
+                                    : `/profile/organizer/${id}`
+                                }
+                                className={classNames(
+                                  active ? 'bg-gray-100' : '',
+                                  'block px-4 py-2 text-sm text-black'
+                                )}
+                              >
+                                Mon Profil
+                              </NavLink>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                onClick={handleLogout}
+                                className={classNames(
+                                  active ? 'bg-gray-100' : '',
+                                  'block px-4 py-2 text-sm text-black'
+                                )}
+                              >
+                                Déconnexion
+                              </a>
+                            )}
+                          </Menu.Item>
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                  ) : (
+                    <>
+                      <NavLink
+                        to={'/subscribe'}
+                        className={
+                          'justify-center px-6 py-2.5 text-sm  text-center text-white whitespace-nowrap bg-rose-500 rounded-[36px] md:px-5 hover:bg-purple-800'
+                        }
+                      >
+                        M'inscrire
+                      </NavLink>
+                      <NavLink
+                        className="justify-center self-center px-5 py-2 text-sm hover:text-rose-500 text-slate-900"
+                        to={'/login'}
+                      >
+                        Connexion
+                      </NavLink>
+                    </>
+                  )}
+                </div>
+                <div className="-mr-2 flex items-center sm:hidden">
+                  {/* Mobile menu button */}
+                  <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <span className="absolute -inset-0.5" />
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
               </div>
             </div>
-          </div>
+            <Disclosure.Panel className="sm:hidden">
+              <div className="space-y-1 pb-3 pt-2 text-black">
+                {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="block border-l-4 hover:border-l-rose-500 hover:rose-indigo-500 hover:bg-rose-50 py-2 pl-3 pr-4 text-base font-medium hover:text-rose-700"
+                >
+                  Évenements
+                </Disclosure.Button>
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="block border-l-4 hover:border-l-rose-500 hover:rose-indigo-500 hover:bg-rose-50 py-2 pl-3 pr-4 text-base font-medium hover:text-rose-700"
+                >
+                  Groupes
+                </Disclosure.Button>
+                <Disclosure.Button
+                  as="a"
+                  href="#"
+                  className="block border-l-4 hover:border-l-rose-500 hover:rose-indigo-500 hover:bg-rose-50 py-2 pl-3 pr-4 text-base font-medium hover:text-rose-700"
+                >
+                  À propos
+                </Disclosure.Button>
+                {islogged === false && (
+                  <>
+                    <Disclosure.Button
+                      as="a"
+                      href="#"
+                      className="block border-l-4 hover:border-l-rose-500 hover:rose-indigo-500 hover:bg-rose-50 py-2 pl-3 pr-4 text-base font-medium hover:text-rose-700"
+                    >
+                      <NavLink to={'/subscribe'}>Inscription</NavLink>
+                    </Disclosure.Button>
+                    <Disclosure.Button className="block border-l-4 hover:border-l-rose-500 hover:rose-indigo-500 hover:bg-rose-50 py-2 pl-3 pr-4 text-base font-medium hover:text-rose-700">
+                      <NavLink to={'/login'}>Connexion</NavLink>
+                    </Disclosure.Button>
+                  </>
+                )}
+              </div>
+              {islogged === true && (
+                <div className="border-t border-gray-200 pb-3 pt-4">
+                  <div className="flex items-center px-4">
+                    <div className="flex-shrink-0">
+                      <img
+                        className="h-10 w-10 rounded-full"
+                        src={`${import.meta.env.VITE_BACK_URL}${image_url}`}
+                        alt="image profil"
+                      />
+                    </div>
+                    <div className="ml-3">
+                      <div className="text-base font-medium text-gray-800">
+                        {name}
+                      </div>
+                      <div className="text-sm font-medium text-gray-500">
+                        {mail}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-3 space-y-1">
+                    <NavLink
+                      to={
+                        role === 'Artiste'
+                          ? `/profile/band/${id}`
+                          : `/profile/organizer/${id}`
+                      }
+                    >
+                      <Disclosure.Button
+                        as="a"
+                        href="#"
+                        className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
+                      >
+                        Mon Profil
+                      </Disclosure.Button>
+                    </NavLink>
+                    <Disclosure.Button
+                      as="a"
+                      onClick={handleLogout}
+                      className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
+                    >
+                      Déconnexion
+                    </Disclosure.Button>
+                  </div>
+                </div>
+              )}
+            </Disclosure.Panel>
+          </>
         )}
-      </header>
-
-      <div className="flex flex-col justify-center pt-3 w-full">
-        <div className="w-full bg-purple-800 border border-purple-800 border-solid min-h-[1px]" />
-      </div>
-
+      </Disclosure>
       {islogged === false ? (
         <div className="hidden xl:flex flex-row justify-center items-center w-full text-base leading-6 whitespace-nowrap bg-white border-b border-solid border-zinc-200 text-neutral-500">
           <div className="flex gap-0 justify-center w-full">
