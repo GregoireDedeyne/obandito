8ee35d71ee4ff3940a7dea75f1d9574f9c3c56a6
gregoireDedeyne <gregoire.dedeyne@gmail.com>
2024-04-13 16:05:50 +0200

    update to jsx

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vueJsx(), react()],
+  optimizeDeps: {
+    exclude: ['buffer'],
+  },
+});
