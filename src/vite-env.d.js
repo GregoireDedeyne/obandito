bf1bf4ae1241de7c95cd2ec9bb73b14e8a197a44
gregoireDedeyne <gregoire.dedeyne@gmail.com>
2024-04-13 17:11:04 +0200

    .env

--- src/vite-env.d.js
+++ src/vite-env.d.js
@@ -1,3 +1,3 @@
-// process.env.VITE_JWT_SECRET =
-//   'klfjlqmkjlmkjsqdflklksdfnlknmksdfnknsdflknknsdflknskdfnmknsdfkn';
-// process.env.VITE_BACK_URL = 'https://obandito-a940f292ca54.herokuapp.com/';
+process.env.VITE_JWT_SECRET =
+  'klfjlqmkjlmkjsqdflklksdfnlknmksdfnknsdflknknsdflknskdfnmknsdfkn';
+process.env.VITE_BACK_URL = 'https://obandito-a940f292ca54.herokuapp.com/';
